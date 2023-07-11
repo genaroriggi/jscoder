80500210848ed0aa16d264a5c2f326b17f2cac85
genaroriggi <genariggi@gmail.com>
2023-07-10 23:43:24 -0300

    2da pre entrega

--- 1/js/index.js
+++ 1/js/index.js
@@ -1,6 +1,18 @@
+function Persona(nombre, apellido, edad){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = edad
+}
+
+const nombre = prompt('Ingresar nombre')
+const apellido = prompt('Ingresar apellido')
+const edad = prompt('Ingresar edad')
+const persona1 = new Persona(nombre, apellido, edad)
+console.log(persona1)
+
 let entrada = prompt ('Cuantos buzos va a comprar? (Maximo 5) ')
 if (entrada >= 1 && entrada < 6) {
-    console.log('El usuario ingresó: '+entrada)
+    console.log(nombre + ' ' + apellido +' ingresó: '+entrada)
 } else {
     console.log('El usuario no ha ingresado una cantidad valida')
 }
@@ -42,5 +54,25 @@ const calcularPrecio = valorProducto => valorProducto * +entrada
 const calcularValorProducto = valor => {
     return valor + calcularPrecio(valor)
 }
-    
 alert(calcularPrecio(4300))
+
+const nombreBuzo = prompt('Ingresar nombre del buzo que busca')
+const buzos = [
+    {nombre: 'Reduced Oversize', precio: 15000},
+    {nombre: 'Bronx', precio: 17000},
+    {nombre: 'Exclusive Oversize', precio: 22000},
+    {nombre: 'Barcode', precio: 22000},
+    {nombre: 'NYCity', precio: 16000},
+]
+let resultado = buzos.find((buzoObject) => buzoObject.nombre === nombreBuzo)
+console.log(resultado)
+
+const buzos2 = [
+    {nombre: 'Reduced Oversize', precio: 15000},
+    {nombre: 'Bronx', precio: 22000},
+    {nombre: 'Exclusive Oversize', precio: 22000},
+    {nombre: 'Barcode', precio: 22000},
+    {nombre: 'NYCity', precio: 16000},
+]
+let resultado1 = buzos2.filter( (buzo2Object) => buzo2Object.nombre.includes('Oversize'))
+console.log(resultado1)
